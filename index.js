69c4b140445a5ae051a48d2265e672924f26e90b
Danielle DeLee <danielledelee@Danielles-MacBook-Pro.local>
2014-03-26 07:08:17 -0700

    startingpoint rebuild

---
+++ index.js
@@ -0,0 +1,16 @@
+var Hapi = require('hapi');
+var host = 'localhost';
+var port = 8000;
+
+var server = Hapi.createServer(host, port);
+
+server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+        directory: { path: './dist', listing: false, index: true }
+    }
+});
+
+server.start();
+console.log('Running at '+host+':'+port);
